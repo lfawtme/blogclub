9a3f20194c68e3d24ce5447e852e02bb1349703d
lfawtme <lfawtme@gmail.com>
2023-05-31 14:49:34 +0330

    startblogclub

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static const defaultFontFamily = 'Avenir';
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final primaryTextColor = Color(0xff0D253C);
+    final secondTextColor = Color(0xff2D4379);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        textTheme: TextTheme(
+          subtitle1: TextStyle(
+            fontFamily: defaultFontFamily,
+            color: secondTextColor,
+          ),
+          headline6: TextStyle(
+              fontFamily: defaultFontFamily,
+              fontWeight: FontWeight.bold,
+              color: primaryTextColor),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 16, 32, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Hi, Fateme!',
+                      style: Theme.of(context).textTheme.subtitle1,
+                    ),
+                    Image.asset(
+                      'assets/img/icons/notification.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(32, 0, 0, 24),
+                child: Text(
+                  'Explore today’s',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
