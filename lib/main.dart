a53d6630a81eb18801c9c8c1565b35af6b99966a
lfawtme <lfawtme@gmail.com>
2023-07-01 13:27:35 +0330

    installingpakage

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,23 @@
 import 'package:blogclub/carousel/carousel_slider.dart';
 import 'package:blogclub/data.dart';
+import 'package:blogclub/gen/assets.gen.dart';
+import 'package:blogclub/gen/fonts.gen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.white,
+    statusBarIconBrightness: Brightness.dark,
+    systemNavigationBarColor: Colors.white,
+    systemNavigationBarIconBrightness: Brightness.dark,
+  ));
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  static const defaultFontFamily = 'Avenir';
   const MyApp({super.key});
   // This widget is the root of your application.
   @override
@@ -22,49 +30,54 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         textButtonTheme: TextButtonThemeData(
             style: ButtonStyle(
-                textStyle: MaterialStateProperty.all(TextStyle(
+                textStyle: MaterialStateProperty.all(const TextStyle(
           fontSize: 14,
           fontWeight: FontWeight.w400,
-          fontFamily: defaultFontFamily,
+          fontFamily: FontFamily.avenir,
         )))),
         primarySwatch: Colors.blue,
         textTheme: const TextTheme(
             subtitle1: TextStyle(
-              fontFamily: defaultFontFamily,
+              fontFamily: FontFamily.avenir,
               color: secondTextColor,
               fontWeight: FontWeight.w200,
               fontSize: 18,
             ),
             subtitle2: TextStyle(
-              fontFamily: defaultFontFamily,
+              fontFamily: FontFamily.avenir,
               color: primaryTextColor,
               fontWeight: FontWeight.w400,
               fontSize: 14,
             ),
             headline6: TextStyle(
-                fontFamily: defaultFontFamily,
+                fontFamily: FontFamily.avenir,
                 fontWeight: FontWeight.bold,
                 color: primaryTextColor,
                 fontSize: 18),
             headline4: TextStyle(
-                fontFamily: defaultFontFamily,
+                fontFamily: FontFamily.avenir,
                 fontSize: 24,
                 fontWeight: FontWeight.w700,
                 color: primaryTextColor),
             headline5: TextStyle(
-              fontFamily: defaultFontFamily,
+              fontFamily: FontFamily.avenir,
               fontSize: 20,
               color: primaryTextColor,
               fontWeight: FontWeight.w700,
             ),
             bodyText2: TextStyle(
-                fontFamily: defaultFontFamily,
+                fontFamily: FontFamily.avenir,
                 color: secondTextColor,
-                fontSize: 14)),
+                fontSize: 14),
+            caption: TextStyle(
+                fontFamily: FontFamily.avenir,
+                fontSize: 10,
+                fontWeight: FontWeight.w700,
+                color: Color(0xff7B8BB2))),
       ),
       home: Stack(
         children: [
-          Positioned.fill(child: const HomeScreen()),
+          const Positioned.fill(child: HomeScreen()),
           Positioned(bottom: 0, right: 0, left: 0, child: _BottomNavigation())
         ],
       ),
@@ -95,11 +108,7 @@ class HomeScreen extends StatelessWidget {
                       'Hi, Fateme!',
                       style: themeData.textTheme.subtitle1,
                     ),
-                    Image.asset(
-                      'assets/img/icons/notification.png',
-                      width: 32,
-                      height: 32,
-                    ),
+                    Assets.img.icons.notification.image(width: 32, height: 32),
                   ],
                 ),
               ),
@@ -116,7 +125,7 @@ class HomeScreen extends StatelessWidget {
               ),
               const _CategoryList(),
               const _PostList(),
-              SizedBox(
+              const SizedBox(
                 height: 32,
               ),
             ],
@@ -421,7 +430,7 @@ class _Post extends StatelessWidget {
                   Text(
                     post.caption,
                     style: const TextStyle(
-                      fontFamily: MyApp.defaultFontFamily,
+                      fontFamily: FontFamily.avenir,
                       fontWeight: FontWeight.w700,
                       fontSize: 14,
                       color: Color(0xff376AED),
@@ -486,7 +495,7 @@ class _Post extends StatelessWidget {
 class _BottomNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: 85,
       child: Stack(
         children: [
@@ -496,17 +505,77 @@ class _BottomNavigation extends StatelessWidget {
             bottom: 0,
             child: Container(
               height: 65,
-              decoration: BoxDecoration(color: Colors.white),
+              decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                BoxShadow(
+                    blurRadius: 20,
+                    color: const Color(0xff2D2D2D).withOpacity(0.3)),
+              ]),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  BottomNavigationItems(
+                      iconFileName: 'Home.png',
+                      activeIconFileName: 'Home.png',
+                      title: 'Home'),
+                  BottomNavigationItems(
+                      iconFileName: 'Articles.png',
+                      activeIconFileName: 'Articles.png',
+                      title: 'Articles'),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  BottomNavigationItems(
+                      iconFileName: 'Search.png',
+                      activeIconFileName: 'Search.png',
+                      title: 'Search'),
+                  BottomNavigationItems(
+                      iconFileName: 'Menu.png',
+                      activeIconFileName: 'Menu.png',
+                      title: 'Menu'),
+                ],
+              ),
             ),
           ),
           Center(
             child: Container(
               width: 65,
-              color: Color(0xff376AED),
+              height: 85,
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: 65,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(32.5),
+                    color: const Color(0xff376AED),
+                    border: Border.all(color: Colors.white, width: 3)),
+                child: Image.asset('assets/img/icons/plus.png'),
+              ),
             ),
           )
         ],
       ),
+    );
+  }
+}
+
+class BottomNavigationItems extends StatelessWidget {
+  final String iconFileName;
+  final String activeIconFileName;
+  final String title;
+
+  const BottomNavigationItems(
+      {super.key,
+      required this.iconFileName,
+      required this.activeIconFileName,
+      required this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset('assets/img/icons/$iconFileName'),
+        const SizedBox(height: 4),
+        Text(title, style: Theme.of(context).textTheme.caption),
+      ],
     );
   }
 }
