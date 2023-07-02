a0486370682677e26894272018b1a067dd508dba
lfawtme <lfawtme@gmail.com>
2023-07-02 11:46:00 +0330

    startingonboarding

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -7,14 +7,41 @@ class OnBoardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
     return Scaffold(
+      backgroundColor: themeData.colorScheme.background,
       body: SafeArea(
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 32, bottom: 32),
-              child: Assets.img.background.onboarding.image(),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 32, bottom: 8),
+                child: Assets.img.background.onboarding.image(),
+              ),
             ),
+            Container(
+              height: 324,
+              decoration: BoxDecoration(
+                  color: themeData.colorScheme.surface,
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 20,
+                      color: Colors.black.withOpacity(0.1),
+                    )
+                  ],
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(32),
+                    topRight: Radius.circular(32),
+                  )),
+              child: Column(
+                children: [
+                  Container(
+                    height: 60,
+                    child: Row(),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
